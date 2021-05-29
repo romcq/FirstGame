bf9a5fd860851b76ae74df9e1716956b53f8d926
romcq <nl.pastuxow@gmail.com>
2021-05-29 15:55:49 +0500

    Почти Готово
    Добавлены картинки, улучшено управление

---
+++ Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstGame
+{
+    class Player
+    {
+        public int platformX;
+        public int platformY;
+        public int ballX;
+        public int ballY;
+        public int score;
+        public int lives;
+        public int directionX = 0;
+        public int directionY = 0;
+        public int platformAnnimationFrame = 0;
+    }
+}
